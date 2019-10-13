8c93d01240fc748cacc5dee95edf0c384589c6a1
FLeX <ahjflex@gmail.com>
2019-10-13 15:35:18 +0200

    Initial commit

---
+++ UIHiddenTextRemover.lua
@@ -0,0 +1,9 @@
+local origActionStatus_DisplayMessage = ActionStatus_DisplayMessage;
+
+ActionStatus_DisplayMessage = function(text)
+    if text == string.format(UI_HIDDEN, GetBindingKey("TOGGLEUI")) then
+        return
+    else
+        origActionStatus_DisplayMessage(text);
+    end
+end
